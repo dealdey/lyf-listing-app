63f6f81f1c06c50f790d6b1aa362fd50fdbf5fa5
Ayoola Ajebeku <ayz4sci@yahoo.com>
2015-09-16 14:37:06 +0100

    LYF-539: Implement verify user when add business button clicked

--- app/src/main/java/ng/lyf/lyflisting/fragments/main/MoneyFragment.java
+++ app/src/main/java/ng/lyf/lyflisting/fragments/main/MoneyFragment.java
@@ -1,5 +1,6 @@
 package ng.lyf.lyflisting.fragments.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
@@ -8,7 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import ng.lyf.lyflisting.Constants;
 import ng.lyf.lyflisting.R;
+import ng.lyf.lyflisting.activities.AccountHelperActivityWithFragment;
 import ng.lyf.lyflisting.activities.HomeActivityWithFragment;
 import ng.lyf.lyflisting.utils.animationHelper.RippleEffect;
 
@@ -20,6 +23,7 @@ public class MoneyFragment extends ListFragment {
     private View                        viewGroupAddButton;
     private View                        rootView;
     private View                        emptyView;
+    private boolean                     isUserValidated = false;
     private ListView                    listView;
     private HomeActivityWithFragment    activity;
 
@@ -59,5 +63,11 @@ public class MoneyFragment extends ListFragment {
         });
     }
 
-    private void addBusiness() {}
+    private void addBusiness() {
+        if(!isUserValidated){
+            Intent intent = new Intent(activity, AccountHelperActivityWithFragment.class);
+            intent.putExtra(Constants.FRAGMENT_TAG, Constants.VERIFY_PHONE_FRAGMENT);
+            startActivity(intent);
+        }
+    }
 }
